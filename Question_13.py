cfeee3145af5a11a1e2e5c5e94e03fbd9644ea10
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-06-30 14:37:56 -0300

    Create Question_13.py

---
+++ Question_13.py
@@ -0,0 +1,17 @@
+s = 'python'
+i = 4
+f = 4.5
+b = True
+t = 1,2,3
+l = [1,2,3]
+c = {1,2,3}
+d = {"one":'um', "two":'dois'} 
+
+print(type(s)) # >> str
+print(type(i)) # >> int
+print(type(f)) # >> float
+print(type(b)) # >> bool
+print(type(t)) # >> tuple
+print(type(l)) # >> list
+print(type(c)) # >> set
+print(type(d)) # >> dict
