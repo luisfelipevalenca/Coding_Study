0eae9fbd95cdf10e192a1b31f875bc9e10922403
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-01 17:52:04 -0300

    Create Question_28.py

---
+++ Question_28.py
@@ -0,0 +1,8 @@
+list = [1, 2, 3]
+# list.sort()    #  [1, 2, 3] -> ordena a lista em in-place, mas não de maneira inversa
+# list.pop()     #   [1, 2] -> vai retirar o último elemento da lista
+# list.remove(1) # [2, 3] ->remove da lista o elmeneto especificado
+list.reverse()   # [3, 2, 1] -> inverte a inverte a ordem dos elementos
+print(list)
+
+# >> [3, 2, 1]
