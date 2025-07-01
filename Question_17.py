7bd96131be5c8dfc04b039c7b7c5c3168eade2ee
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-01 15:53:52 -0300

    Create Question_17.py

---
+++ Question_17.py
@@ -0,0 +1,4 @@
+L = [i for i in range(-1, -2)] # gera uma lista vazia pois o critério de parada (stop) é menor que o valor do critério de entrada (start) range(start, stop)
+print (L)
+
+# >> [] (a lista contem zero elementos)
