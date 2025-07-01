dda5737022c9505b50a9b031d1b5d419b9f506f5
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-01 13:55:00 -0300

    Create Question_16.py

---
+++ Question_16.py
@@ -0,0 +1,5 @@
+data = {'a': 1, 'b':2, 'c':3} # cria uma dicionário com key-values
+print(data['a', 'b']) # tenta acessar os elementos em 'data' usando tuplas 'a' e 'b'. Como a tupla ('a', 'b') não existe como chave no dicionário de dados, um KeyError é gerado.
+
+# >> KeyError: ('a', 'b')
+#  The code is erroneous.
