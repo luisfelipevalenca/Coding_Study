df23131ae7914e582252f9ab58d4356c51a5644e
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-02 14:32:20 -0300

    Create Question_32.py

---
+++ Question_32.py
@@ -0,0 +1,9 @@
+List1 = [1, 2, 3] # lista composta por números inteiros
+
+List1[0], List1[2] = List1[2], List1[0] # [3,2,1]
+
+# List1.reverse() a operação anterior teria o mesmo efeito ordenando a lista utilizando método reverse()
+
+print(List1)
+
+# >> [3, 2, 1]
