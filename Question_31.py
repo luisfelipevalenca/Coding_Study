f9c1526fb065770285d43b70f6edaa0e9a7ced5d
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-02 14:26:20 -0300

    Create Question_31.py

---
+++ Question_31.py
@@ -0,0 +1,9 @@
+def func(n): # cria uma função que vai operacionalizar o valor da variável de entrada 'total'
+  return total - n
+
+total = 4 
+total = func(2) # total = 4 - 2 = 2
+total = func(1) # total = 2 - 1 = 1
+print(total)
+
+# >> 1
