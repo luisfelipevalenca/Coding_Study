17796b0bfdf0598463bca44f80b39e162e9af8f2
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-02 14:35:30 -0300

    Create Question_33.py

---
+++ Question_33.py
@@ -0,0 +1,14 @@
+a = "True"    # str
+b = True      # bool
+c = 1         # int
+d = 1.45266   # float
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# >> <class 'str'>
+# >> <class 'bool'>
+# >> <class 'int'>
+# >> <class 'float'>
