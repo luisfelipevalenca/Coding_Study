80ab0dc3eadc8cb81cce8a6ae62444e4a05ab462
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-02 15:06:28 -0300

    Update Question_38.py

--- Question_38.py
+++ Question_38.py
@@ -1,4 +1,4 @@
-print(20//3**2)
+print(2//3**2)
 
 # 1 - 3 ** 2 = 9 (primeiro calcula-se a parte exponencial)
 # 2 - 2 // 9 = 0 (floor division vai printar a parte inteira 2/9 = 0.222... ou seja, imprime 0)
