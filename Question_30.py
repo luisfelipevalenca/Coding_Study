8cc52d60f79a982a340d07a18778592b855d56ca
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-02 14:20:06 -0300

    Create Question_30.py

---
+++ Question_30.py
@@ -0,0 +1,15 @@
+i = 1 
+while i < 2: 
+  i += 2 # i = i + 2 
+         # i = 1
+         # i = 1 + 2 = 3 -> >> *
+  print("*", end="")
+
+else:
+  # em i = 3 -> >> *
+  print("*")
+
+# o laço roda uma vez, printa um asterisco, depois segue para o próximo bloco,
+# ele printa na mesma lista pois é tem o argumento 'end=""'
+
+# >> **
