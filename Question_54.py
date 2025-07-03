c73dcc634dc4767543dc21a642286fb600ff7f31
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-03 14:37:22 -0300

    Create Question_54.py

---
+++ Question_54.py
@@ -0,0 +1,7 @@
+num = 3 // 2 * 3 # (3 // 2) * 3 = 1 * 3 = 3
+if num > 3: # o bloco não é executado pois num = 3
+   print('Hello World')
+else:
+  print('Something different')
+
+# >> Something different
