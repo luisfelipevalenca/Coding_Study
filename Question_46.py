54bbb847d97b6fd7eddd8c58891a3b791c59d771
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-03 12:26:30 -0300

    Create Question_46.py

---
+++ Question_46.py
@@ -0,0 +1,4 @@
+message = "She said, \"Python is fun!\"" # o \ \ sobrepassam o "", perttindo o uso das aspas ao printar a mensagem sem que incorra em erros
+print(message)
+
+# >> She said, "Python is fun!"
