5a10ee5390dcf0d1a3e4860263dfeb2cf95cb682
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-03 12:27:47 -0300

    Create Question_50.py

---
+++ Question_50.py
@@ -0,0 +1,4 @@
+List1 = [[10, 20], [30, 50, 60], [80, 90]] # cria uma lista composta por outras sub-listas
+print(List1[2][1]) # corresponde ao segundo elemento da lista corresponde ao index 2. Ou seja 90.
+
+# >> 90
