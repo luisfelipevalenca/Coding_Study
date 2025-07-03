14566b210a15c2e3169b13df4d2775684d297630
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-03 12:25:54 -0300

    Create Question_44.py

---
+++ Question_44.py
@@ -0,0 +1,7 @@
+i = 1
+while i < 3:
+    print (i) # não existe incremento ou condição que atualize o estado da variável inicial, então teremos um loop infinito
+
+# >> i
+# >> i
+# >> ... (it will be infinite loop)
