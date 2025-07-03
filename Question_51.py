ac2cfad2d5724186cc6c8496f84ab027ab9d9e3a
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-03 12:28:23 -0300

    Create Question_51.py

---
+++ Question_51.py
@@ -0,0 +1,5 @@
+List1 = [10, 20, 30, 40, 50]
+# List1.reverse() a lista pode ser invertida usando o metodo .reverse()
+print(List1[::-1]) # a lista pdoe ser invertida também utilizando o comando onde [::-1] -> vai do primeiro ao ultimo elemento da lista e depois cria uma cópia invertida
+
+# >> [50, 40, 30, 20, 10]
