6af1f135502b988026b063bcc03b0fa54e4820cc
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-04 15:12:50 -0300

    Create Question_62.py

---
+++ Question_62.py
@@ -0,0 +1,11 @@
+num1 = input("enter the first number:")
+num2 = input("enter the second number:")
+num3 = num1 + num2 # apenas concatena os valores de entrada numa nova string
+
+# para que realizar a soma, seria necessário converter as strings para números inteiros ou flutuantes (int(), float())
+
+print(num3)
+
+# enter the first number:123123
+# enter the second number:22131234124
+# 12312322131234124
