9274204bc379fab4037fd641aaef9870e23de363
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-04 13:44:14 -0300

    Create Question_57.py

---
+++ Question_57.py
@@ -0,0 +1,5 @@
+dict = {"Dalsaver": 12, "Aussever": 14, "Londonserver": 16}
+for i in dict.items():
+  print (i[0], end="") # printa todas as chaves na mesma linha de forma concateada
+
+# >> DalsaverAusseverLondonserver
