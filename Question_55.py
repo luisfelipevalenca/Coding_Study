97843610a9256a1d5fbdef345c8a1e4837b44c35
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-04 13:43:23 -0300

    Create Question_55.py

---
+++ Question_55.py
@@ -0,0 +1,6 @@
+fruits = ['Apple', 'Orange', 'Mango'] # cria uma lsita com strings
+for fruit in fruits: # para as strings dentro da lista
+  if fruit != 'Apple': # analisa a condição da string não ser igual a 'Apple'
+     print(fruit, end="") # imprime os conteúdos da lista em ordem, juntos na mesma linha pois a função usa o parametro 'end'
+
+# >> OrangeMango
