a00603d664800f28f9ff59555c1c847e04ca84c1
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-04 15:24:22 -0300

    Create Question_64.py

---
+++ Question_64.py
@@ -0,0 +1,11 @@
+# convertendo 101010 de binario para decimal
+print (int('101010',2))
+# >> 42
+
+# convertendo 101010 de binario para hexadecimal
+print (hex(int('101010',2)))
+# >> 0x2a
+
+# convertendo 101010 de binario para octadecimal
+print(oct(int('101010', 2)))
+# >> 0o52
