e0e762d1ee0c50aee21281b22a6093ee74297d61
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-05 20:40:39 -0300

    Create Question_70.py

---
+++ Question_70.py
@@ -0,0 +1,14 @@
+fruits = ['Apple', 'Banana', 'Gym', 10]
+
+for fruit in fruits:
+  print (type(fruit)) # printa o tipo de dado em cada item da lista
+  print(fruit) # printa o elemento abaixo de sua classificação
+
+# <class 'str'>
+# Apple
+# <class 'str'>
+# Banana
+# <class 'str'>
+# Gym
+# <class 'int'>
+# 10
