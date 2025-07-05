cc661b97708de1e0e8fd10832edd4ab76aba2844
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-05 19:54:34 -0300

    Create Question_69.py

---
+++ Question_69.py
@@ -0,0 +1,6 @@
+marking_period_scores = [96, 95, 99, 97]
+
+avg_mp_scores = (sum (marking_period_scores)) // len(marking_period_scores) # sum(...) / len () = 96.75 , mas como a questao quer o módulo, fica-se apenas com a parte real, ou seja 96
+print(avg_mp_scores)
+
+# >> 96
