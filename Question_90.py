ab777713db69b02a380a4ba7a76235ff9cbed885
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-06 12:49:51 -0300

    Create Question_90.py

---
+++ Question_90.py
@@ -0,0 +1,6 @@
+data = set([1, 2, 2, 3, 3, 3, 4, 4, 4, 4]) # cria uma lista composta por inteiros, como a função set não 
+                                           # permite elementos duplicados a lista será {1, 2, 3, 4}
+
+print(len(data)) # o tamanho da do subset da lista 'data' será 4
+
+# >> 4
