87d3051464f648f14fdd76d2a6e1a8ef221c1605
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-06 14:55:28 -0300

    Create Question_99.py

---
+++ Question_99.py
@@ -0,0 +1,9 @@
+# You develop a Python application for your company.
+# A list named employees contains 200 employee names, the last five being company management. You need to slice the list to display all employees excluding management.
+# Which code segments can you use? (Choose two.)
+
+# A. employees[1:-5]
+# B. employees[0:-5] (OK)
+# C. employees[:-5]  (OK)
+# D. employees[0:-4]
+# E. employees[1:-4]
