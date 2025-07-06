514cc8b0657edc20cb7faf44c5e02de6483e0a9e
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-06 13:03:25 -0300

    Create Question_92.py

---
+++ Question_92.py
@@ -0,0 +1,4 @@
+a = [1, 2, 3, 4, 5] # cria uma lista sequenciada de elementos int
+print(a[3:0:-1]) # i[3] = 4, i[2] = 3, i[1] = 2 -> como o step é um elemento no sentindo inverso, a[3:0:-1] = [4, 3, 2]
+
+# >> [4, 3, 2]]
