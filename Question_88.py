10641db24c7b3888f50b7bc92df7bbfe02f347f7
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-06 12:40:24 -0300

    Create Question_88.py

---
+++ Question_88.py
@@ -0,0 +1,10 @@
+data1 = (1, 2)
+data2 = (3, 4)
+
+[print(sum(x)) for x in [data1 + data2]] # faz uma list comprehension, uma maneira concisa de criar listas.
+                                         # para cada elemento presente nas tuplas concatenadas, realizada a soma e printa o resultado
+# data1 + data2 = ((1, 2),(3, 4))
+# sum(x) = 1 + 2 + 3 + 4 = 10 
+
+# >> 10
+# >> [None]
