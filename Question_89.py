1f34d32c48632cb75d1ee059bde287d36382ef8c
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-06 12:44:55 -0300

    Create Question_89.py

---
+++ Question_89.py
@@ -0,0 +1,7 @@
+vals = [0, 1, 2] # cria uma lista de ints
+vals.insert(0, 1) # insere no index 0, o elemento int 1 -> vals = [1, 0, 1, 2]
+del vals[1] # deleta o elemento do index 1 -> vals [1, 1, 2]
+
+print(sum(vals)) # realiza a soma dos elementos dentro da lista: 1 + 1 + 2 = 4 
+
+# >> 4
