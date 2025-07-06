5bcb01cdb9275a54f2a76913142225bb65356707
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-05 21:26:39 -0300

    Create Question_74.py

---
+++ Question_74.py
@@ -0,0 +1,5 @@
+d = {'A': 1, 'B': 2, 'C': 3} # cria um dicionário com key-values
+d.clear() # limpa os dados presentes no dicionário 'd'
+print(d) # printa o dicionário pós método '.clear()'
+
+# >> {}
