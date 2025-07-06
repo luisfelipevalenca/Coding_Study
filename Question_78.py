72d15e7d52a8f3e35afb1fc8bc8ba04627afa0b1
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-05 21:54:21 -0300

    Create Question_78.py

---
+++ Question_78.py
@@ -0,0 +1,10 @@
+data1 = '1', '2' # cria uma tupla  de elementos ('1','2') com tamanho dois composta por strings
+print (data1)
+data2 = ('3', '4') # cria uma nova tupla de elementos ('3','4') com tamanho dois, composta por strings
+print(data2)
+
+print(data1 + data2) # ('1','2','3','4')
+
+# >> ('1', '2')
+# >> ('3', '4')
+# >> ('1','2','3','4')
