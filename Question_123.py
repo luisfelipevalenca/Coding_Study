49a62ddf172c61989a4deccdba6e2e4714552d10
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-08 11:02:43 -0300

    Create Question_123.py

---
+++ Question_123.py
@@ -0,0 +1,9 @@
+data = [1, 5, 10, 19, 55, 30, 55, 99]
+
+data.pop(5)         # tira o elemento de index 5
+data.remove(19)     # remove o elemento cujo valor seja 19
+data.remove(55)     # remove o elemento cujo valor 55 
+data.remove(55)     # remove o segundo elemento cujo valor seja 55
+print(data)
+
+# [1, 5, 10, 99]
