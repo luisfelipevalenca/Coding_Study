2178b15f15a8d97f86ba9d57d5f15ca92f4577d0
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-09 13:55:31 -0300

    Create Question_143.py

---
+++ Question_143.py
@@ -0,0 +1,8 @@
+x = input()
+y = input()
+
+print(x + y) # concatena as strings x e y, caso quisesse operacionalizar a soma, deveria ser previamente convertidara para float ou int
+print(type(x+y))
+
+# >> 24
+# >> <class 'str'>
