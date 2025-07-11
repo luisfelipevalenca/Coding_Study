cd0590b91697b7ef74285854c751abd07b6080a5
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-11 13:08:22 -0300

    Create Question_163.py

---
+++ Question_163.py
@@ -0,0 +1,5 @@
+# Which of the following statements best describes the purpose of the __init__ method in a Python class?
+#  A. It is used to return a string representation of the object.
+#  B. It initializes the object's attributes when an instance is created. (OK)
+#  C. It is invoked when a class is inherited.
+#  D. It is used to delete an instance of the object.
