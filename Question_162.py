b45f456d8729bb045d4ddbefc648f66aa1b612af
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-11 13:07:11 -0300

    Create Question_162.py

---
+++ Question_162.py
@@ -0,0 +1,10 @@
+def check_value(x):
+    return "Positive" if x > 0 else "Negative" if x < 0 else "Zero"
+
+print(check_value(-10)) 
+print(check_value(0))
+print(check_value(10))
+
+# >> Negative
+# >> Zero
+# >> Positive
