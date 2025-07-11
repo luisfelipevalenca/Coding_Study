d0171bd3938fa32c1f37dfeb8ad7a624a62b2b23
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-11 12:58:44 -0300

    Create Question_159.py

---
+++ Question_159.py
@@ -0,0 +1,9 @@
+def func(n):
+    return (n + 1) if n % 2 == 0 else (n - 1)
+    # caso (if) n for par, retorna n+1
+    # caso (if) n for impar, retorna n-1
+
+# print(func(6))
+# >> 7
+print(func(4))
+# >> 5
