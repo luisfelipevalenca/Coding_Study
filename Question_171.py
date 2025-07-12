e4bfeda92ddd42654f3f84d03ac203b4b72de454
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-12 16:18:56 -0300

    Create Question_171.py

---
+++ Question_171.py
@@ -0,0 +1,8 @@
+"""When using keyword arguments in a function, which of the following is a valid way to call the function?
+ A. my_function(x=1, 2)
+ B. my_function(2, x=1)(OK)
+ C. my_function(2, y=3)
+
+Keyword arguments can be specified in any order. However, positional arguments must appear before
+keyword arguments.
+"""
