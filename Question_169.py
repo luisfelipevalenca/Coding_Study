f6e21653ef5d29bd908d2ebbffc10d8e62a6c08e
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-12 16:18:17 -0300

    Create Question_169.py

---
+++ Question_169.py
@@ -0,0 +1,5 @@
+"""How do you define a function that can take an arbitrary number of positional arguments and keyword
+ arguments?
+ A. def my_function(*args, kwargs): (OK)
+ B. def my_function(args, kwargs):
+ C. def my_function(*args, **args):"""
