ca9eba0a9986e92690af7e6f30847c39bb1e740e
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-13 01:00:30 -0300

    Create Question_182.py

---
+++ Question_182.py
@@ -0,0 +1,8 @@
+x =  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+x[::2] = 10, 20, 30, 40, 50, 60
+
+print(x)
+
+# >> This code is erroneous
+
+# >> ValueError: attempt to assign sequence of size 6 to extended slice of size 5
