e6c8cb0d7d50c90f49a105cf470fb767d6e4e6bf
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-14 09:06:21 -0300

    Create Question_189.py

---
+++ Question_189.py
@@ -0,0 +1,7 @@
+# converting a list of tuples into a dictionary
+tuples_list = [('name', 'alice'), ('age', 21)]
+dict_from_tuples = dict(tuples_list)
+
+print(dict_from_tuples)
+
+# >> {'name': 'alice', 'age': 21
