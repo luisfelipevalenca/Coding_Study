23b7b1972958440b7f928003d1febc6a449d5b21
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-15 08:47:05 -0300

    Create Question_200.py

---
+++ Question_200.py
@@ -0,0 +1,10 @@
+# Lambda Function: are small anonymous functions defined useing the lambda keyword.
+# They are useful for simple operations in a single line
+
+# 1. Lambda Function (a lambda function that adds two numbers together)
+add = lambda a,b: a+b
+
+result = add(3,4)
+print(result)
+
+# >> 7
