19f47537712319319a012584c7f0521c92dde695
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-15 08:46:06 -0300

    Create Question_197.py

---
+++ Question_197.py
@@ -0,0 +1,10 @@
+# Scope of Variables
+
+# 1. Local Variables (variables defined within a function)
+def greet():
+    message = 'Hello, Python!'
+    print(message)
+
+greet()
+
+# >> Hello, Python!
