3a51a57692785ad05f515245de8131f103eef817
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-15 09:05:59 -0300

    Create Question_204.py

---
+++ Question_204.py
@@ -0,0 +1,11 @@
+def area_of_rectangle(width, height):
+    return width * height
+
+width = 5
+height = 10
+
+area = area_of_rectangle(width, height)
+
+print(f'Area of rectangle is:{area}')
+
+# >> Area of rectangle is:50
