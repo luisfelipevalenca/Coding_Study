eb2dd5161b0c14ee0675835c368931ba40798405
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-17 12:32:49 -0300

    Create Question_221.py

---
+++ Question_221.py
@@ -0,0 +1,12 @@
+import random
+
+random_number = random.randint(1, 10)
+print(random_number)
+
+fruits = ['apple', 'banana', 'orange']
+random_fruit = random.choice(fruits)
+print(random_fruit)
+
+# >> 5
+# >> orange
+  
