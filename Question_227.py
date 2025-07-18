fabacc9db50ab8b9f98ec01316abfd0346471197
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-18 08:39:25 -0300

    Create Question_227.py

---
+++ Question_227.py
@@ -0,0 +1,6 @@
+# Which of the following statements is true about lists in Python?
+
+# a. lists are immutable
+# b. lists can contain elements of different data types. (OK)
+# c. lists cannot be nested
+# d. lists are created using parentheses
