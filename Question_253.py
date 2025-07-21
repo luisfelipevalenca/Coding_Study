ea616f4b01434876dc7784d43246fa3b7da11bf6
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-21 08:18:17 -0300

    Create Question_253.py

---
+++ Question_253.py
@@ -0,0 +1,7 @@
+"""
+Which of the following statements will remove the key 'name' from the dictionary student?
+a. student.remove('name')
+b. del student['name'] (OK)
+c. student.pop('name')
+d. student.delete('name')
+"""
