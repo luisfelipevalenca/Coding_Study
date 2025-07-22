7fc289f7a92ed089bf85aad1b60ef78e58ff51ef
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-22 11:07:38 -0300

    Create Question_268.py

---
+++ Question_268.py
@@ -0,0 +1,6 @@
+"""What does the len() function do?
+a. returns the number of elements in a list
+b. returns the length of a string
+c. returns the number of keys in a dictionary
+d. all of the above (OK)
+"""
