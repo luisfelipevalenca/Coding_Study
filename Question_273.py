4b155479e5cae2f9ed40857b9593a2ca1e6ac8d8
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-23 10:19:29 -0300

    Create Question_273.py

---
+++ Question_273.py
@@ -0,0 +1,9 @@
+"""
+Which of the following statements will correctly iterate through each character in a string s?
+
+a. for char in s:
+b. while char in s:
+c. for char in range(len(s))
+d. while char in range(len(s))
+
+"""
