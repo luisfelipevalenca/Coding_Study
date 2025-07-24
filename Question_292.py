04738b117f553e87f3392dfc80ef6d7b554bef99
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-24 09:20:02 -0300

    Create Question_292.py

---
+++ Question_292.py
@@ -0,0 +1,9 @@
+"""
+
+Which of the following statements will correctly check if a key exists in a dictionary?
+a. if 'key' in dict:
+b. if dict.contains('key'):
+c. if 'key' exists in dict: (OK)
+d. if dict.has_key('key'):
+
+"""
