1e3e9be226ff38c149e4e5aa38d0ae826924f1af
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-24 09:18:09 -0300

    Create Question_286.py

---
+++ Question_286.py
@@ -0,0 +1,9 @@
+"""
+
+Which of the following correct syntax for defing a function in Python?
+a. def function_name:
+b. function function_name:
+c. def function_name(): (OK)
+d. function function_name():
+
+"""
