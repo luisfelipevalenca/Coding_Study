b2e9c1aea8fbdecda70025d7128ea2da5109dc98
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-25 08:28:41 -0300

    Create Question_302.py

---
+++ Question_302.py
@@ -0,0 +1,9 @@
+"""
+
+What does the append() method do in Python?
+a. adds an item to the beginning of a list
+b. adds an item to the end of a list (OK)
+c. removes an item from a list
+d. sorts the items in a list
+
+"""
