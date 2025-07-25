e65b5cffdcca5324acc30ecdc4d065ba99014ef7
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-25 08:29:19 -0300

    Create Question_304.py

---
+++ Question_304.py
@@ -0,0 +1,5 @@
+my_dict = {'a':1, 'b':2}
+print(my_dict.get('c', 'default')) # usa o método .get() para pegar o valor associado à chave 'c' do dicionário.
+                                   # neste caso, a chave 'c' não existe em my_dict, então o valor padrão é retornado como default
+
+# >> default
