e9a4d86e6fa12a5e630ee907a1a1b21e23f827d5
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-27 15:20:23 -0300

    Create Question_312.py

---
+++ Question_312.py
@@ -0,0 +1,5 @@
+print('My code\nis\nwritten\nin\npython'.split())
+print(''.join('My code\nis\nwritten\nin\npython'.split()))
+
+# >> ['My', 'code', 'is', 'written', 'in', 'python']
+# >> Mycodeiswritteninpython
