0e9d16dd30e143bc170997f41b8c5a3ac7f724b5
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-07-27 15:28:16 -0300

    Create Question_313.py

---
+++ Question_313.py
@@ -0,0 +1,8 @@
+s1 = 'my code is written in python'
+s2 = s1.split() # ['my', 'code', 'is', 'written', 'in', 'python']
+                  # i[0] = my, i[1] = code, ... i[5] = python
+                  # i[-1] = python, i[-2] = in, ..., i[-6] = my
+print(s2[-2])
+
+
+# >> in
